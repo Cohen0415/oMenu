369ed725cc7b7d1fec5eff15d5ee6feeddca3d1e
Cohen0415 <1033878279@qq.com>
2025-05-31 18:27:19 +0800

    实现菜单解析和菜单交互功能

--- u-boot/drivers/plugins/omenu/cmd_omenu.h
+++ u-boot/drivers/plugins/omenu/cmd_omenu.h
@@ -1,18 +1,16 @@
 #ifndef _CMD_OMENU_H
 #define _CMD_OMENU_H
 
+#define MAX_SELECTION 	128
+#define MAX_PATH 		256
+
+#define STORE_DEV		"mmc"
 
 #define MAX_CFG_LEN 32      
 typedef struct configs {
     char mmc_dev_num[MAX_CFG_LEN];
     char mmc_partition[MAX_CFG_LEN];
     char directory_name[MAX_CFG_LEN];
-}configs_t;
-
-#define MAX_ENTRIES 64
-typedef struct omenu_entry {
-    char name[128];
-    bool is_dir;
-} omenu_entry_t;
+} configs_t;
 
 #endif
